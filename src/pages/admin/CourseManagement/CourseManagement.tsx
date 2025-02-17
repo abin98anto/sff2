03f31e6de84342ae24de2ebb2e75aa4aa480be0b
abin98anto <abin98anto@gmail.com>
2025-02-17 16:59:16 +0530

    -course management dummy created.

--- src/pages/admin/CourseManagement/CourseManagement.tsx
+++ src/pages/admin/CourseManagement/CourseManagement.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CourseManagement = () => {
   return <div>CourseManagement</div>;
 };
--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -4,6 +4,7 @@ import AdminDashboard from "../pages/admin/AdminDashboard/AdminDashboard";
 import AdminLayout from "../components/admin/AdminLayout/AdminLayout";
 import PageNotFound from "../components/common/PageNotFound/PageNotFound";
 import Categories from "../pages/admin/CategoryManagement/CategoryManagement";
+import CourseManagement from "../pages/admin/CourseManagement/CourseManagement";
 
 const AdminRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AdminRoutes = () => {
       <Route element={<AdminLayout />}>
         <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/category-management" element={<Categories />} />
+        <Route path="/course-management" element={<CourseManagement />} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
